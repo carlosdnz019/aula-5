a407b35ec03b1526123eb5dec41830d9e47c14c5
carlosdnz019 <carlosdonizeti94@gmail.com>
2026-09-18 19:28:43 -0300

    Add files via upload
    atividade 5

---
+++ atv aula 5/projeto-bone/script.js
@@ -0,0 +1,18 @@
+const imagens = [
+  "https://images.unsplash.com/photo-1520975916090-3105956dac38?q=80&w=1200",
+  "https://images.unsplash.com/photo-1516820580870-3f66c730d990?q=80&w=1200",
+  "https://images.unsplash.com/photo-1507838153414-b4b713384a76?q=80&w=1200"
+];
+let indice = 0;
+
+function nextSlide() {
+  indice = (indice + 1) % imagens.length;
+  document.getElementById('carrossel-img').src = imagens[indice];
+}
+
+function prevSlide() {
+  indice = (indice - 1 + imagens.length) % imagens.length;
+  document.getElementById('carrossel-img').src = imagens[indice];
+}
+
+// Removi a função comprar() do JavaScript pois agora o PHP faz isso através do submit do form!
